e27f35d56920ecae58a74a91ff2206e2451721c7
Nicolas Brûlez <nicolas.brulez@parrot.com>
2020-04-29 11:51:36 +0200

    [DEV] Do not build shared library if TARGET_FORCE_STATIC is set

--- atom.mk
+++ atom.mk
@@ -5,4 +5,9 @@ LOCAL_MODULE := libyuv
 LOCAL_CATEGORY_PATH := libs
 LOCAL_DESCRIPTION := YUV scaling and conversion library
 LOCAL_EXPORT_LDLIBS := -lyuv
+
+ifeq ("$(TARGET_FORCE_STATIC)","1")
+	LOCAL_CMAKE_CONFIGURE_ARGS := -DYUV_SHARED=OFF
+endif
+
 include $(BUILD_CMAKE)
